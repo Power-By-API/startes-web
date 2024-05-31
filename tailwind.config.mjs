d7aa3e2225461a57a7f506086999664b5c850e00
Ismael Treviño <ismolahiena@gmail.com>
2024-05-31 19:13:44 +0200

    style(Design Styles): :lipstick: Color Scheme
    Creado variables con esquema de colores, con propuesta de colores v1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-	  extend: {
-		fontFamily: {
-		  sans: ['Kanit', 'sans-serif'],
+		extend: {
+			fontFamily: {
+				sans: ['Kanit', 'sans-serif'],
+			},
+			colors: {
+				"primary-color": "var(--primary-color)",
+				"secondary-color": "var(--secondary-color)"
+			}
 		},
-	  },
 	},
 	plugins: [],
-  }
+}
